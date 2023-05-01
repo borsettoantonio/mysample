c29005a0f5454de888dc3a806232e4c85f69581c
antonio <borsettoantonio@gmai.com>
2023-05-01 09:46:04 +0200

    Esempio di gestione dello stato con provider - 1

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Flutter: Esempio di gestione dello stato con setState()
+# Flutter: Esempio di gestione dello stato con provider - 1
+01.05.2023
 
-Esempio tratto dal libro "Flutter Complete Reference" di Alberto Miola a pag.290.
+Esempio tratto dal libro "Flutter Complete Reference" di Alberto Miola a pag.299.
 
-La chiamata a setState provoca sempre la ricostruzione di WidgetB,
-ma non quella di WidgetC che è definito const.
+In MaterialApp creo una istanza di ChangeNotifierProvider che a sua volta
+crea un CounterModel e lo mette a disposizione dei widget discendenti.
 
 Definendo WidgetC non-const allora anch'esso viene ricostruito a ogni chiamata di setState.
+
+Mettendo la chiamata Provider.of<CounterModel>(context) nel widget DemoPage
+al cambiamento di CounterModel vengono ricostruiti tutti i widget a partire da DemoPage.
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:mysample/counter_model.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'State management 1';
+  static const String _title = 'State management 2';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: DemoPage(),
+    return MaterialApp(
+      home: ChangeNotifierProvider(
+        create: (context) => CounterModel(),
+        child: const DemoPage(),
+      ),
     );
   }
 }
 
-class DemoPage extends StatefulWidget {
+class DemoPage extends StatelessWidget {
   const DemoPage({super.key});
-
-  @override
-  _DemoPageState createState() => _DemoPageState();
-}
-
-class _DemoPageState extends State<DemoPage> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() => _counter++);
-  }
-
-  void _decrement() {
-    setState(() => _counter--);
-  }
 
   @override
   Widget build(BuildContext context) {
     debugPrint("_DemoPageState");
+
+    final counter = Provider.of<CounterModel>(context);
     return Scaffold(
         appBar: AppBar(title: const Text('LayoutBuilder Example')),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children:[
-            WidgetB(s:"$_counter"),
-            const WidgetC(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                    onPressed: _increment,
-                    child: const Text("+1",
-                        style: TextStyle(color: Colors.green, fontSize: 25))),
-                Text("$_counter",
-                    style: const TextStyle(
-                      fontSize: 30,
-                    )),
-                TextButton(
-                    onPressed: _decrement,
-                    child: const Text("-1",
-                        style: TextStyle(color: Colors.red, fontSize: 25)))
-              ],
-            )],
+            children: [
+              WidgetB(s: "${counter.currentCount}"),
+              WidgetC(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                      onPressed: () => counter.increment(),
+                      child: const Text("+1",
+                          style: TextStyle(color: Colors.green, fontSize: 25))),
+                  Text("${counter.currentCount}",
+                      style: TextStyle(color: Colors.amber, fontSize: 25)),
+                  TextButton(
+                      onPressed: counter.decrement,
+                      child: const Text("-1",
+                          style: TextStyle(color: Colors.red, fontSize: 25)))
+                ],
+              )
+            ],
           ),
         ));
   }
@@ -69,16 +59,15 @@ class _DemoPageState extends State<DemoPage> {
 
 class WidgetB extends StatelessWidget {
   const WidgetB({super.key, required this.s});
-
-  final String s; 
+  final String s;
 
   @override
   Widget build(BuildContext context) {
     debugPrint("WidgetB");
     return Text(s,
-                    style: const TextStyle(
-                      fontSize: 30,
-                    ));
+        style: const TextStyle(
+          fontSize: 30,
+        ));
   }
 }
 
@@ -89,8 +78,8 @@ class WidgetC extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint("WidgetC");
     return const Text("pippo",
-                    style: TextStyle(
-                      fontSize: 30,
-                    ));
+        style: TextStyle(
+          fontSize: 30,
+        ));
   }
 }
